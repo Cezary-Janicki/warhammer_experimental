90b2ba375077318d7af0355d77fc6a8651495999
Cezary-Janicki <94046566+Cezary-Janicki@users.noreply.github.com>
2023-02-28 14:28:27 +0100

    added more data fetching functions

---
+++ app/(components)/dataFetching/Wargear_list.js
@@ -0,0 +1,24 @@
+import path from "path";
+const fs = require("fs");
+const Papa = require("papaparse");
+
+const csvFilePath = path.join(
+  process.cwd(),
+  "public/database/Wargear_list.csv"
+);
+// Function to read csv which returns a promise so you can do async / await.
+
+// const readCSV = async (filePath) => {
+export default async function DataFetch() {
+  const csvFile = fs.readFileSync(csvFilePath);
+  const csvData = csvFile.toString();
+  return new Promise((resolve) => {
+    Papa.parse(csvData, {
+      header: true,
+      complete: (results) => {
+        // console.log("Complete", results.data.length, "records.");
+        resolve(results.data);
+      },
+    });
+  });
+}
