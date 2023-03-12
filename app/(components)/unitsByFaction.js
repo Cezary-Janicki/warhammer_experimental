caa8f11c1dc70b828afbd7e139ef365ee05ca07c
Cezary-Janicki <94046566+Cezary-Janicki@users.noreply.github.com>
2023-03-12 17:50:51 +0100

    Static Page generatcion works properly now

---
+++ app/(components)/unitsByFaction.js
@@ -0,0 +1,27 @@
+// // here i need to make an array that would be as follows
+// // 1. faction id 2. faction name 3. unit name 4.unit id 5. unit role
+// // so that I can use it to generate pages
+
+// import {Factions} from "../(components)/dataFetching/Factions"
+// import {Data}
+import { MainFactions } from "../../../(components)/dataFetching/Factions";
+import { getDatasheetByFaction } from "../../../(components)/dataFetching/Datasheets";
+
+async function fetchFactions() {
+  const data = await MainFactions();
+  return data;
+}
+async function fetchDatasheetByFaction({ props: string }) {
+  const data = await getDatasheetByFaction(props);
+  return data;
+}
+
+export default async function unitsByFaction() {
+  const factions = await fetchFactions();
+  // const units = ()=>{let data= await fetchDatasheetByFaction() return data}
+  // factions.map((faction)=>{
+  // const units = await fetchDatasheetByFaction()
+
+  // })
+  // here i need an function that would map each item in fetch factions and for each item it would map all datasheets and filter out the faction specific ones
+}
--- app/factions/[faction]/[unit]/page.tsx
+++ app/factions/[faction]/[unit]/page.tsx
@@ -1,37 +1,31 @@
 import { MainFactions } from "../../../(components)/dataFetching/Factions";
-// import Datasheets from "../../../(components)/dataFetching/Datasheets";
+import Datasheets from "../../../(components)/dataFetching/Datasheets";
 import { getDatasheetByFaction } from "../../../(components)/dataFetching/Datasheets";
 
-async function fetch(props) {
+async function fetch(props: string) {
   const data = await getDatasheetByFaction(props);
   return data;
 }
-
+async function createArray() {
+  const units = await Datasheets();
+  return units.map((item: { faction_id: string; name: string }) => ({
+    faction: `${item.faction_id}`,
+    unit: `${item.name}`,
+  }));
+}
 export async function generateStaticParams() {
-  const data = await MainFactions();
-  let factionNames = [];
-  data.map((item, index) => {
-    factionNames.push(item.name);
-  });
-  // return factionNames.map((factionName) => ({
-  //   slug: factionName.slug,
-  // }));
-  return [
-    { faction: "Orks", unit: "Boyz" },
-    { faction: "Orks", unit: "Warboss" },
-    { faction: "SpaceMarines", unit: "Dreadnought" },
-  ];
+  const factions = await MainFactions();
+  const xxx = await createArray(); // here faction and unit is a string
+  return xxx; // but not on return
 }
 
 export default async function Page({
   params,
 }: {
-  params: { faction: any; unit: any };
+  params: { faction: string; unit: string };
 }) {
-  const { faction, unit } = params;
-  console.log("unit ", unit);
+  const { faction, unit } = await params;
   const data = await fetch("ORK");
-  // console.log("data", data);
 
   return (
     <>
