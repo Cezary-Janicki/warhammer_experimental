b6c3bd050abda2b2377a6cc1bbe56f495ba7c012
Cezary-Janicki <94046566+Cezary-Janicki@users.noreply.github.com>
2023-07-28 15:57:38 +0200

    Playing with basic tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -14,14 +14,58 @@ export default async function Home() {
   const factions = await getMainFactions();
   return (
     <>
-      <p> Hello next 13!</p>
-      {factions.map((faction: any, factionIndex: number) => {
+      <p className="text-3xl font-bold underline"> Hello next 13!</p>
+<div className="flex">
+  
+            <div>
+              <h1 className="underline">Imperium</h1>
+              <ul>
+                  <li className="list-none indent-6"><Link  href="/factions/AS">Adepta Sororitas</Link></li>
+                  <li className="list-none indent-6"><Link href="/factions/AC">Adeptus Custodes</Link></li>
+                  <li className="list-none indent-6"><Link href="/factions/AdM">Adeptus Mechanicus</Link></li>
+                  <li className="list-none indent-6"><Link href="/facitons/AoI">Agents of the Imperium</Link></li>
+                  <li className="list-none indent-6"><Link href="/factions/AM">Astra Militarum</Link></li>
+                  <li className="list-none indent-6"><Link href="/factions/GK">Grey Knights</Link></li>
+                  <li className="list-none indent-6"><Link href="/factions/IK">Imperial Knights</Link></li>
+                  <li className="list-none indent-6"><Link href="/factions/RT">Rogue Traders</Link></li>
+                  <li className="list-none indent-6"><Link href="/factions/SM">Space Marines</Link></li>
+                  <li className="list-none indent-6"><Link href="/factions/TL">Titan Legions</Link></li>
+              </ul>
+            </div>
+<div>
+              <h1 className="underline">Chaos</h1>
+                <ul>
+                    <li className="list-none indent-6"><Link href="/factions/CD">Chaos Daemons</Link></li>
+                    <li className="list-none indent-6"><Link href="/factions/CK">Chaos Knights</Link></li>
+                    <li className="list-none indent-6"><Link href="/factions/CSM">Chaos Space Marines</Link></li>
+                    <li className="list-none indent-6"><Link href="/factions/DG">Death Guard</Link></li>
+                    <li className="list-none indent-6"><Link href="/factions/HTL">Heretic Titan Legions</Link></li>
+                    <li className="list-none indent-6"><Link href="/factions/RaH">Renegades and Heretics</Link></li>
+                    <li className="list-none indent-6"><Link href="/factions/TS">Thousand Sons</Link></li>
+                    <li className="list-none indent-6"><Link href="/factions/WE">World Eaters</Link></li>
+                </ul>
+</div>
+<div>
+              <h1 className="underline">Xenos</h1>
+                  <ul>
+                    <li className="list-none indent-6"><Link href="/factions/AE">Aeldari</Link></li>
+                    <li className="list-none indent-6"><Link href="/factions/DRU">Drukhari</Link></li>
+                    <li className="list-none indent-6"><Link href="/factions/GSC">Genestealer Cults</Link></li>
+                    <li className="list-none indent-6"><Link href="/factions/LoV">Leagues of Votann</Link></li>
+                    <li className="list-none indent-6"><Link href="/factions/NEC">Necrons</Link></li>
+                    <li className="list-none indent-6"><Link href="/facitons/ORK">Orks</Link></li>
+                    <li className="list-none indent-6"><Link href="/factions/TAU">T’au Empire</Link></li>
+                    <li className="list-none indent-6"><Link href="/factions/TYR">Tyranids</Link></li>
+                  </ul>
+</div>
+</div>
+      {/* {factions.map((faction: any, factionIndex: number) => {
         return (
           <h1 key={factionIndex}>
             <Link href={`/factions/${faction.id}`}>{faction.name}</Link>
           </h1>
         );
-      })}
+      })} */}
     </>
   );
 }
