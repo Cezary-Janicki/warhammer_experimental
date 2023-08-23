c0e9caaaf18676f06beb80dba617377cf7c28059
Cezary-Janicki <94046566+Cezary-Janicki@users.noreply.github.com>
2023-08-23 16:42:24 +0200

    Added use reducer for role counting in navbar

--- app/(components)/pageComponents/navbar/Popup.tsx
+++ app/(components)/pageComponents/navbar/Popup.tsx
@@ -4,20 +4,37 @@ export default function NavbarPopup(props: any) {
   let units = props.units;
   let roles = props.roles;
   let faction = props.faction;
+
+  function countRoles() {
+    let result = units.reduce(function (acc, val) {
+      if (faction == val.faction_id) {
+        acc[val.role] = (acc[val.role] || 0) + 1;
+      }
+      return acc;
+    }, {});
+    return result;
+  }
+  // i need to reduce here to find out what roles are being used and which shouldnt be displayed
   return (
     <>
+      {console.log("count roles", Object.keys(countRoles()).includes("Troops"))}
       <div>
         <ul className="dropdown-content absolute text-gray-700 pt-1">
           {roles.map((role: string, index: number) => {
             return (
               <li className="dropdown">
-                <a
-                  className="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
-                  href="#"
-                  key={index}
-                >
-                  {role}
-                </a>
+                {Object.keys(countRoles()).includes(role) == true ? (
+                  <a
+                    className="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
+                    href="#"
+                    key={index}
+                  >
+                    {role}
+                  </a>
+                ) : (
+                  <></>
+                )}
+
                 <ul className="dropdown-content absolute hidden text-gray-700 pl-5 ml-24 -mt-10">
                   {units.map((unit: any) => {
                     if (unit.role == role && faction == unit.faction_id) {
