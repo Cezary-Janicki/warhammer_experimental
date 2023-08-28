c0b1e148148bedd29edf2ba1ac8884dd563c8ae4
Cezary-Janicki <94046566+Cezary-Janicki@users.noreply.github.com>
2023-08-28 18:25:01 +0200

    Added a points button

--- app/(components)/pageComponents/UnitStatsTable.tsx
+++ app/(components)/pageComponents/UnitStatsTable.tsx
@@ -3,46 +3,60 @@ import * as React from "react";
 
 export default function UnitStatsTable(props: any) {
   const models = props.models;
+  console.log("model", models);
   return (
     <>
       <div
         className={
-          "grid grid-cols-15 bg-neutral-500 border border-neutral-800 rounded-md p-2.5 drop-shadow-md text-neutral-50 font-semibold"
+          "grid grid-cols-16 bg-neutral-500 border border-neutral-800 rounded-md p-2.5 drop-shadow-md text-neutral-50 font-semibold"
         }
       >
         <div className={"ml-2.5 col-span-1"}>No</div>
         <div className={"col-span-3"}>Unit Name</div>
-        <div className={"col-span-1"}>M</div>
-        <div className={"col-span-1"}>WS</div>
-        <div className={"col-span-1"}>BS</div>
-        <div className={"col-span-1"}>S</div>
-        <div className={"col-span-1"}>T</div>
-        <div className={"col-span-1"}>W</div>
-        <div className={"col-span-1"}>A</div>
-        <div className={"col-span-1"}>Ld</div>
-        <div className={"col-span-1"}>Sv</div>
-        <div className={"col-span-2"}>Base</div>
+        <div className={"col-span-1"}>Cost</div>
+        <div className={"col-span-1 text-right"}>M</div>
+        <div className={"col-span-1 text-right"}>WS</div>
+        <div className={"col-span-1 text-right"}>BS</div>
+        <div className={"col-span-1 text-right"}>S</div>
+        <div className={"col-span-1 text-right"}>T</div>
+        <div className={"col-span-1 text-right"}>W</div>
+        <div className={"col-span-1 text-right"}>A</div>
+        <div className={"col-span-1 text-right"}>Ld</div>
+        <div className={"col-span-1 text-right"}>Sv</div>
+        <div className={"col-span-2 text-right"}>Base</div>
       </div>
 
       {models.map((model: any, index: number) => (
         <div
           key={model.name}
           className={
-            "grid grid-cols-15 border border-neutral-300 rounded-md p-2.5 drop-shadow-md  odd:bg-neutral-200 even:bg-neutral-50 "
+            "grid grid-cols-16 border border-neutral-300 rounded-md p-2.5 drop-shadow-md  odd:bg-neutral-200 even:bg-neutral-50 "
           }
         >
           <div className={"ml-2.5 col-span-1 font-semibold"}> {model.line}</div>
           <div className={"col-span-3 font-semibold"}> {model.name}</div>
-          <div className={"col-span-1"}> {model?.M}</div>
-          <div className={"col-span-1"}> {model?.WS}</div>
-          <div className={"col-span-1"}> {model?.BS}</div>
-          <div className={"col-span-1"}> {model?.S}</div>
-          <div className={"col-span-1"}> {model?.T}</div>
-          <div className={"col-span-1"}> {model?.W}</div>
-          <div className={"col-span-1"}> {model?.A}</div>
-          <div className={"col-span-1"}> {model?.Sv}</div>
-          <div className={"col-span-1"}> {model?.Ld}</div>
-          <div className={"col-span-2"}> {model?.base_size}</div>
+          {model.name.length >= 1 ? (
+            <div
+              className={
+                "col-span-1 bg-red-700 text-center text-neutral-50 font-bold rounded-full"
+              }
+            >
+              {model?.Cost} pts
+            </div>
+          ) : (
+            <div className="col-span-1"></div>
+          )}
+
+          <div className={"col-span-1 text-right"}> {model?.M}</div>
+          <div className={"col-span-1 text-right"}> {model?.WS}</div>
+          <div className={"col-span-1 text-right"}> {model?.BS}</div>
+          <div className={"col-span-1 text-right"}> {model?.S}</div>
+          <div className={"col-span-1 text-right"}> {model?.T}</div>
+          <div className={"col-span-1 text-right"}> {model?.W}</div>
+          <div className={"col-span-1 text-right"}> {model?.A}</div>
+          <div className={"col-span-1 text-right"}> {model?.Sv}</div>
+          <div className={"col-span-1 text-right"}> {model?.Ld}</div>
+          <div className={"col-span-2 text-right"}> {model?.base_size}</div>
         </div>
       ))}
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,8 @@ module.exports = {
       gridTemplateColumns: {
         // 15 column grid
         15: "repeat(15, minmax(0, 1fr))",
+        // 16 column grid
+        16: "repeat(16, minmax(0, 1fr))",
         // 20 column grid
         20: "repeat(20, minmax(0, 1fr))",
       },
